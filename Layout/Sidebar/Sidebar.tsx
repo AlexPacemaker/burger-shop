51f06c1b9c4eae4798cae37770a8c0fa530c57ca
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-22 15:59:01 +0300

    upd

--- Layout/Sidebar/Sidebar.tsx
+++ Layout/Sidebar/Sidebar.tsx
@@ -2,6 +2,8 @@ import styles from "./Footer.module.scss";
 import cn from "classnames";
 import { ISidebar } from "./Sidebar.props";
 
-export const Sidebar = ({ ...props }: ISidebar): JSX.Element => {
+const Sidebar = ({ ...props }: ISidebar): JSX.Element => {
   return <div {...props}>Sidebar</div>;
 };
+
+export default Sidebar;
