f1047273483c9d06a4e544cedc6bf41f73505050
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-22 15:58:05 +0300

    upd

--- Layout/Footer/Footer.tsx
+++ Layout/Footer/Footer.tsx
@@ -2,6 +2,8 @@ import styles from "./Footer.module.scss";
 import cn from "classnames";
 import { IFooter } from "./Footer.props";
 
-export const Footer = ({ ...props }: IFooter): JSX.Element => {
+const Footer = ({ ...props }: IFooter): JSX.Element => {
   return <div {...props}>Footer</div>;
 };
+
+export default Footer;
